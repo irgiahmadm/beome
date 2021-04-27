d941bfd5fadc46f7fbc9c4307ad6276993ea886f
irgiahmadm <irgiahmadmaulana@gmail.com>
2021-04-27 20:38:49 +0700

    fix function report account and post

--- app/src/main/java/com/beome/ui/report/ReportRepository.kt
+++ app/src/main/java/com/beome/ui/report/ReportRepository.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.beome.model.*
 import com.beome.utilities.NetworkState
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
@@ -24,21 +27,7 @@ class ReportRepository(private val coroutineScope : CoroutineScope) {
                 try {
                     stateReportFeedback.postValue(NetworkState.LOADING)
                     val reportDetail = Firebase.firestore.collection("reported_feedback_detail")
-                    reportDetail.whereEqualTo("username", report.username).get().addOnSuccessListener {
-                        if(it.documents.isEmpty()){
-                            reportDetail.add(report)
-                        }else{
-                            for (document in it){
-                                if(document["reportReason"] != report.reportReason){
-                                    reportDetail.add(report)
-                                    break
-                                }
-                            }
-                        }
-                    }.addOnFailureListener {
-                        Log.d("err_get_report_detail", it.message.toString())
-                        stateReportFeedback.postValue(NetworkState.FAILED)
-                    }.await()
+                    checkReportAlreadyExist(report, reportDetail, stateReportFeedback)
                     Firebase.firestore.runTransaction { transaction ->
                         val reportedFeedbackRef = Firebase.firestore.collection("reported_feedback").document(reportedFeedback.feedback.idFeedback)
                         if(transaction.get(reportedFeedbackRef).exists()){
@@ -62,18 +51,23 @@ class ReportRepository(private val coroutineScope : CoroutineScope) {
         coroutineScope.launch {
             withContext(Dispatchers.IO){
                 try {
-                    stateReportFeedback.postValue(NetworkState.LOADING)
+                    stateReportAccount.postValue(NetworkState.LOADING)
+                    val reportDetail = Firebase.firestore.collection("reported_account_detail")
+                    checkReportAlreadyExist(report, reportDetail, stateReportAccount).await()
                     Firebase.firestore.runTransaction { transaction ->
                         val reportedAccountRef = Firebase.firestore.collection("reported_account").document(reportedAccount.user.authKey)
-                        val reportDetail = Firebase.firestore.collection("reported_account_detail").document()
-                        transaction.set(reportedAccountRef, reportedAccount)
-                        transaction.set(reportDetail, report)
-
+                        if(transaction.get(reportedAccountRef).exists()){
+                            val user = transaction.get(reportedAccountRef)
+                            val updateCounter = user["counter"] as Long + 1
+                            transaction.update(reportedAccountRef, "counter", updateCounter)
+                        }else{
+                            transaction.set(reportedAccountRef, reportedAccount)
+                        }
                     }.await()
-                    stateReportFeedback.postValue(NetworkState.SUCCESS)
+                    stateReportAccount.postValue(NetworkState.SUCCESS)
                 }catch (e : Exception){
                     Log.d("err_add_report_feedback", e.message.toString())
-                    stateReportFeedback.postValue(NetworkState.FAILED)
+                    stateReportAccount.postValue(NetworkState.FAILED)
                 }
             }
         }
@@ -84,13 +78,17 @@ class ReportRepository(private val coroutineScope : CoroutineScope) {
             withContext(Dispatchers.IO){
                 try {
                     stateReportFeedback.postValue(NetworkState.LOADING)
+                    val reportDetail = Firebase.firestore.collection("reported_post_detail")
+                    checkReportAlreadyExist(report, reportDetail, stateReportFeedback).await()
                     Firebase.firestore.runTransaction { transaction ->
                         val reportedPostRef = Firebase.firestore.collection("reported_post").document(reportedPost.post.idPost)
-                        val reportDetail = Firebase.firestore.collection("reported_post_detail").document()
-
-                        transaction.set(reportedPostRef, reportedPost)
-                        transaction.set(reportDetail, report)
-
+                        if(transaction.get(reportedPostRef).exists()){
+                            val post = transaction.get(reportedPostRef)
+                            val updateCounter = post["counter"] as Long + 1
+                            transaction.update(reportedPostRef, "counter", updateCounter)
+                        }else{
+                            transaction.set(reportedPostRef, reportedPost)
+                        }
                     }.await()
                     stateReportFeedback.postValue(NetworkState.SUCCESS)
                 }catch (e : Exception){
@@ -99,5 +97,23 @@ class ReportRepository(private val coroutineScope : CoroutineScope) {
                 }
             }
         }
+    }
+
+    private fun checkReportAlreadyExist(report : ReportDetail, reportDetail : CollectionReference, networkState: MutableLiveData<NetworkState>) : Task<QuerySnapshot>{
+        return reportDetail.whereEqualTo("username", report.username).get()
+            .addOnSuccessListener {
+                if(it.documents.isEmpty()){
+                    reportDetail.add(report)
+                }else{
+                    for (document in it){
+                        if(document["reportReason"] != report.reportReason){
+                            reportDetail.add(report)
+                            break
+                        }
+                    }
+                }
+            }.addOnFailureListener {
+                networkState.postValue(NetworkState.FAILED)
+            }
     }
 }
