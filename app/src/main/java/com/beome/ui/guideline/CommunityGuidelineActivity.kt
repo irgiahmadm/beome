718ef3da217f7ea75caed46a443260c9e3c10afb
irgiahmadm <irgiahmadmaulana@gmail.com>
2021-05-19 22:43:32 +0700

    delete unused import

--- app/src/main/java/com/beome/ui/guideline/CommunityGuidelineActivity.kt
+++ app/src/main/java/com/beome/ui/guideline/CommunityGuidelineActivity.kt
@@ -2,7 +2,6 @@ package com.beome.ui.guideline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import com.beome.databinding.ActivityCommunityGuidelineBinding
 
